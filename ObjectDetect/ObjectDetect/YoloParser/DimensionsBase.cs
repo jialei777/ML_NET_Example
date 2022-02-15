aa2d77b146dedea164657cee27466d78adde0889
Jialei Chen <jialeichen@microsoft.com>
2022-02-15 14:20:08 -0800

    obj-detect example

---
+++ ObjectDetect/ObjectDetect/YoloParser/DimensionsBase.cs
@@ -0,0 +1,10 @@
+﻿namespace ObjectDetect.YoloParser
+{
+    public class DimensionsBase
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Height { get; set; }
+        public float Width { get; set; }
+    }
+}
