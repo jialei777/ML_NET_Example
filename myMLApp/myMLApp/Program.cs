3d8318269842069e2d321ebeb8593d736951b303
Jialei Chen <jialeichen@microsoft.com>
2022-02-16 12:12:34 -0800

    add an Model Builder example

---
+++ myMLApp/myMLApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using MyMLApp;
+// Add input data
+var sampleData = new SentimentModel.ModelInput()
+{
+    Col0 = "This restaurant was wonderful."
+};
+
+// Load model and predict output of sample data
+var result = SentimentModel.Predict(sampleData);
+
+// If Prediction is 1, sentiment is "Positive"; otherwise, sentiment is "Negative"
+string sentiment = result.Prediction == 1 ? "Positive" : "Negative";
+Console.WriteLine($"Text: {sampleData.Col0}\nSentiment: {sentiment}");
