abae12b86615b2fbe4bc48ebe87acb45a7debcd8
Jialei Chen <jialeichen@microsoft.com>
2022-06-23 12:07:44 -0700

    a simple grpc example

---
+++ GrpcDemo/GrpcClient/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Net.Client;
+using GrpcService;
+
+
+var input = new HelloRequest { Name = "Jialei" };
+
+//var channel = GrpcChannel.ForAddress("http://localhost:5251");
+var channel = GrpcChannel.ForAddress("https://localhost:7251");
+var client = new Greeter.GreeterClient(channel);
+
+var reply = await client.SayHelloAsync(input);
+
+Console.WriteLine(reply.Message);
+
+Console.ReadLine();
